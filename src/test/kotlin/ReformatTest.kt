2f93752e1ac930ab6afa9d021a7de2bd2b910497
Darya Razumova <d.razumova@yandex.ru>
2020-04-05 00:19:35 +0300

    Tests from examples

--- src/test/kotlin/ReformatTest.kt
+++ src/test/kotlin/ReformatTest.kt
@@ -1,9 +1,25 @@
 package com.test.filtermap
 
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
 class ReformatTest {
+    private fun checkResultsOnEquals(initialLine: String, list: List<Int>) {
+        val line = reformat(initialLine)
+        checkFormat(line)
+        assertEquals(apply(initialLine, list), apply(line, list))
+    }
+
+    private fun checkResultsOnEquals(initialLine: String, minValue: Int = -100, maxValue: Int = 100) {
+        checkResultsOnEquals(initialLine, (minValue..maxValue).toList())
+    }
+
+    private fun checkFormat(result: String) {
+        assertTrue(result.matches("filter\\{[^{}]*}%>%map\\{[^{}]*}".toRegex()))
+    }
+
     @Test
     fun syntaxErrorOnEmptyStringTest() {
         assertThrows<SyntaxErrorException> {
@@ -51,5 +67,20 @@ class ReformatTest {
         assertThrows<TypeErrorException> {
             reformat("filter{((1<element)<10)}")
         }
+    }
+
+    @Test
+    fun filterTwiceExample() {
+        checkResultsOnEquals("filter{(element>10)}%>%filter{(element<20)}")
+    }
+
+    @Test
+    fun filterAfterMapExample() {
+        checkResultsOnEquals("map{(element+10)}%>%filter{(element>10)}%>%map{(element*element)}")
+    }
+
+    @Test
+    fun emptyFilterResultExample() {
+        checkResultsOnEquals("filter{(element>0)}%>%filter{(element<0)}%>%map{(element*element)}")
     }
 }
