fe564426bd44608e5ddbabf310b5e5bd8d711c40
Darya Razumova <d.razumova@yandex.ru>
2020-04-05 08:34:34 +0300

    Constant arithmetic expressions computing in reformat

--- src/main/kotlin/com/test/filtermap/visitors/CollectCallsVisitor.kt
+++ src/main/kotlin/com/test/filtermap/visitors/CollectCallsVisitor.kt
@@ -6,6 +6,7 @@ import com.test.parser.CallChainParser
 
 /**
  * Call chain visitor reformatting all actions with list to one filter call and one map call.
+ * Computes all arithmetic expressions with constant values.
  */
 class CollectCallsVisitor : CallChainBaseVisitor<Unit>() {
     private var mapExpression = "element"
@@ -17,33 +18,49 @@ class CollectCallsVisitor : CallChainBaseVisitor<Unit>() {
             throw TypeErrorException()
         }
         val modifiedCondition = ctx.expression().accept(substituteVisitor)
-        addFilterExpression(modifiedCondition)
+        addFilterExpression(modifiedCondition.result)
     }
 
     override fun visitMapCall(ctx: CallChainParser.MapCallContext) {
         if (!ctx.expression().type().hasIntResult()) {
             throw TypeErrorException()
         }
-        mapExpression = ctx.expression().accept(substituteVisitor)
+        mapExpression = ctx.expression().accept(substituteVisitor).result
     }
 
-    private inner class Substitute : CallChainBaseVisitor<String>() {
-        override fun visitBinaryExpression(ctx: CallChainParser.BinaryExpressionContext): String {
+    private data class SubstituteResult(val isConstant: Boolean, val result: String)
+
+    private inner class Substitute : CallChainBaseVisitor<SubstituteResult>() {
+
+        override fun visitBinaryExpression(ctx: CallChainParser.BinaryExpressionContext): SubstituteResult {
             if (!ctx.hasCorrectTypes()) {
                 throw TypeErrorException()
             }
 
             val left = ctx.left.accept(this)
             val right = ctx.right.accept(this)
-            return "($left${ctx.operation().text}$right)"
+            val leftResult = left.result
+            val rightResult = right.result
+
+            if (ctx.type() == ExpressionType.INT && left.isConstant && right.isConstant) {
+                val result = when(ctx.operation().text) {
+                    "+" -> leftResult.toInt() + rightResult.toInt()
+                    "-" -> leftResult.toInt() - rightResult.toInt()
+                    "*" -> leftResult.toInt() * rightResult.toInt()
+                    else -> throw TypeErrorException()
+                }
+                return SubstituteResult(true, result.toString())
+            }
+
+            return SubstituteResult(false, "($leftResult${ctx.operation().text}$rightResult)")
         }
 
-        override fun visitConstantExpression(ctx: CallChainParser.ConstantExpressionContext): String {
-            return ctx.NUMBER().text
+        override fun visitConstantExpression(ctx: CallChainParser.ConstantExpressionContext): SubstituteResult {
+            return SubstituteResult(true, ctx.NUMBER().text)
         }
 
-        override fun visitItExpression(ctx: CallChainParser.ItExpressionContext): String {
-            return mapExpression
+        override fun visitItExpression(ctx: CallChainParser.ItExpressionContext): SubstituteResult {
+            return SubstituteResult(false, mapExpression)
         }
     }
 
